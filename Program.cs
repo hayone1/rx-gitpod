257d847b0b4c7427ba4754e651eac7bbc2f5b4e3
Boluwatife Morawo <boluwatife.morawo@stu.cu.edu.ng>
2022-12-25 08:01:54 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,12 +1,54 @@
-﻿using System;
+using System.Reactive;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+// var numbers = new NumbersObservable(5);
+// var subscription = numbers.SubscribeConsole("numbers");
 
-namespace dotnetcore
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+// ChatClient chatClient = new();
+// var connection = chatClient.Connect("guest", "guest");
+
+// IObservable<string> observableConnection = new ObservableConnection(connection);
+
+// var subscription = observableConnection.SubscribeConsole("receiver");
+// chatClient.Connect("guest", "pa$$w0rd")
+//             .ToObservable()
+//             .SubscribeConsole("receiver");
+
+// var deferredMessages = chatClient.ObserveMessagesDeferred("user", "password");
+// var sub1 = deferredMessages.SubscribeConsole("ok");
+// var sub2 = deferredMessages.SubscribeConsole("yo");
+
+MockWPF EventPatternTest = new();
+EventPatternTest.scanner.OnWiFiFound();
+
+EventPatternTest.scanner.OnExWiFiFOund();
+EventPatternTest.scanner.OnConnected();
+
+
+var _toObservable = new EnumToObservable();
+//_toObservable.myobservable.SubscribeConsole("get the names");
+// _toObservable.NumbersAndThrow().ToObservable().SubscribeConsole("will_Throw_Error");
+//_toObservable.names.Subscribe(new ConsoleObserver<string>("Subscribe Directly"));
+// _toObservable.ConcatMessages();
+//_toObservable.ObservableToEnumerable();
+//_toObservable.ObservableToList();
+// _toObservable.ObservableToDict();
+// _toObservable.ObservableToLookup();
+// _toObservable.GenerateNUmbers();
+
+MagicalPrimeGenerator generator = new();
+// foreach(var prime in generator.GeneratePrimesNaive(10)){
+//     // Console.WriteLine("New number ");
+//     Console.Write("{0},",prime);
+// }
+generator.GeneratePrimes(5)
+        .Timestamp()
+        .SubscribeConsole("Prime Generator");
+Console.WriteLine("Generation done");
+
+SearchEngine engine = new();
+engine.search("Observable Search").SubscribeConsole("Start Searching");
+Console.ReadLine();
+
